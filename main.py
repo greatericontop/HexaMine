5ecb287f0f2f0011ccad0a8997c0d0c920dacf03
greatericontop <greateric@protonmail.com>
2022-11-19 22:26:43 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""HexaMine is a game where you have to find the mines in a hexagonal grid."""
+
+# <License to be added later.>
+
+import pygame
+
+
+def main():
+    pygame.init()
+    print('hello world')
+
+
+if __name__ == '__main__':
+    main()
